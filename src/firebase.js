06c0d0703ef14037d80b78c61db486dcb284e328
JoelPoah <joel.21@ichat.sp.edu.sg>
2022-09-12 16:46:41 +0800

    added home image

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBUtqclS9HIHqQBJL7RcgHlKvWf_zyMNtk",
+    authDomain: "clone-f3a9c.firebaseapp.com",
+    projectId: "clone-f3a9c",
+    storageBucket: "clone-f3a9c.appspot.com",
+    messagingSenderId: "378542377694",
+    appId: "1:378542377694:web:34b3d1436b809c99a0104f",
+    measurementId: "G-H8QNV556HJ"
+  };
